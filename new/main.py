35b92ed946597f421751109327eb3d41109615bb
rex <220922869@qq.com>
2019-03-14 15:09:42 +0800

    Initial commit

---
+++ new/main.py
@@ -0,0 +1,13 @@
+from urllib import  request
+
+import  time
+
+while True:
+    cmd = request.urlopen("http://api.itmojun.com/pc/cmd/get?id=dj").read()
+    cmd = cmd.decode("gbk")
+
+    if cmd!="":
+        print(cmd)
+        time.sleep(3)
+    else:
+        time.sleep(1)
